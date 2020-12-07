cad4cb1c472f18627ac00abc8ed1a3c3576d5ef4
NuiS4ncE <nuis4nce@hotmail.com>
2020-12-07 16:06:13 +0200

    exercise 3 done

--- viikko6/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -17,18 +17,56 @@ public class Main {
             System.out.println(player);
         }
 
+        System.out.println("");
 
         Matcher y = new And(
                 new Not( new HasAtLeast(1, "goals") ),
                 new PlaysIn("NYR")
         );
 
+        System.out.println("");
+
+        for (Player player : stats.matches(y)) {
+            System.out.println(player);
+        }
+
+        System.out.println("");
+
         Matcher x = new And(
                 new HasFewerThan(1, "goals"),
                 new PlaysIn("NYR")
         );
 
+        for (Player player : stats.matches(x)) {
+            System.out.println(player);
+        }
+
+        System.out.println("");
         System.out.println(stats.matches(new All()).size());
+        System.out.println("");
+
+        Matcher z = new Or( new HasAtLeast(40, "goals"),
+                new HasAtLeast(60, "assists")
+        );
+
+        for (Player player : stats.matches(z)) {
+            System.out.println(player);
+        }
+
+        System.out.println("");
+
+        Matcher a = new And(
+                new HasAtLeast(50, "points"),
+                new Or(
+                        new PlaysIn("NYR"),
+                        new PlaysIn("NYI"),
+                        new PlaysIn("BOS")
+                )
+        );
+
+        for (Player player : stats.matches(a)) {
+            System.out.println(player);
+        }
 
     }
 }
